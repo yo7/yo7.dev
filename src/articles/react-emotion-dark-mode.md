ee1d100c7d79b10259ee33b0f80d0cd8a06a5f16
yo7 <bloom6445@gmail.com>
2019-10-06 17:08:11 +0900

    article page

--- src/articles/react-emotion-dark-mode.md
+++ src/articles/react-emotion-dark-mode.md
@@ -2,6 +2,7 @@
 path: "/articles/react-emotion-darkmode"
 date: "2019-10-01"
 title: "Reactとemotionでダークモードを実現する"
+img: "dark-mode.png"
 ---
 
-hoge
+test
--- src/templates/ArticleTemplate.tsx
+++ src/templates/ArticleTemplate.tsx
@@ -1,5 +1,11 @@
 import * as React from "react"
 import { graphql } from "gatsby"
+import { Global, css } from "@emotion/core"
+import SEO from "../components/seo"
+import { globalStyles } from "../styles/global"
+import { ArrowLeft, Share2 } from "react-feather"
+import { Image } from "../components/Image"
+import { checkPropTypes } from "prop-types"
 
 type Props = {
   data: {
@@ -8,6 +14,7 @@ type Props = {
       frontmatter: {
         title: string
         date: string
+        img: string
       }
     }
   }
@@ -16,17 +23,34 @@ type Props = {
 export default ({ data }: Props) => {
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark
+
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
+    <>
+      <Global styles={globalStyles} />
+      <SEO title="Page two" />
+      <div>
+        <Header />
+        <Eyecatch title={frontmatter.title} img={frontmatter.img} />
         <div
-          className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
+          css={css`
+            max-width: 680px;
+            margin: 0 auto;
+            padding: 0.5rem 1rem;
+
+            p {
+              line-height: 1.5;
+            }
+
+            p > code,
+            li > code {
+              padding: 0.25rem 0.4rem 0.15rem;
+              border-radius: 3px;
+            }
+          `}
         />
       </div>
-    </div>
+    </>
   )
 }
 
@@ -38,7 +62,75 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         path
         title
+        img
       }
     }
   }
 `
+
+const Header: React.FC = () => {
+  return (
+    <div
+      css={css`
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        z-index: 1;
+      `}
+    >
+      <div
+        css={css`
+          padding: 0.6rem 0.8rem;
+          display: flex;
+          justify-content: space-between;
+          max-width: 960px;
+          margin: 0 auto;
+        `}
+      >
+        <ArrowLeft color="#fff" size={30} />
+        <Share2 color="#fff" size={28} />
+      </div>
+    </div>
+  )
+}
+
+export const Eyecatch: React.FC<{ title: string; img: string }> = props => {
+  return (
+    <div
+      css={css`
+        position: relative;
+        background-color: #999;
+        > div {
+          max-height: 250px;
+        }
+
+        img {
+          mix-blend-mode: multiply;
+        }
+      `}
+    >
+      <Image file={props.img} />
+      <div
+        css={css`
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          right: 0;
+          margin: auto;
+          max-width: 680px;
+        `}
+      >
+        <h1
+          css={css`
+            color: #fff;
+            margin: 1rem;
+            line-height: 1.25;
+          `}
+        >
+          {props.title}
+        </h1>
+      </div>
+    </div>
+  )
+}
