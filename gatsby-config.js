d4bcd2c99271fc3e25fa359b18a17c830204c72b
yo7 <bloom6445@gmail.com>
2019-10-13 22:17:55 +0900

    icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Seiya Yoshitaka`,
+    description: `Personal site by Seiya Yoshitaka`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -26,13 +26,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `seiya yoshitaka`,
+        short_name: `sy`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1a1a1a`,
+        theme_color: `#1a1a1a`,
         display: `minimal-ui`,
-        icon: `images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `images/icon.png`,
       },
     },
     `gatsby-plugin-typescript`,
