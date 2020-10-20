de419788197ebbfe7a2d2f2ce5796eb4564c9747
yo7 <bloom6445@gmail.com>
2020-10-21 00:38:09 +0900

    Change title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,62 +53,34 @@ export default ({
       >
         <div
           css={css`
-            margin-top: 3rem;
+            margin-top: 2rem;
             padding: 0.5rem 1.5rem;
           `}
         >
           <div>
-            <div
-              css={css`
-                height: 90px;
-                width: 90px;
-
-                @media (min-width: 640px) {
-                  height: 100px;
-                  width: 100px;
-                }
-                float: right;
-                shape-outside: circle(80%);
-
-                img {
-                  border-radius: 50%;
-                }
-              `}
-            >
-              <Image file="profile.png" />
-            </div>
             <h1
               css={css`
                 font-family: Roboto;
-                font-size: 2.2rem;
+                font-size: 1.3rem;
                 margin: 0;
-                letter-spacing: 0.5px;
+                display: inline-block;
+                color: #111;
               `}
             >
-              Seiya Yoshitaka
+              yo7.dev
             </h1>
-            <p
-              css={css`
-                line-height: 1.7;
-                color: #555;
-                margin-top: 1.2rem;
-              `}
-            >
-              Web開発者です。ツール系のアプリをつくるのが好きです。
-              主にモダンなWebアプリの開発について書きます。
-            </p>
           </div>
         </div>
         <div
           css={css`
-            margin-top: 4rem;
+            margin-top: 3rem;
           `}
         >
           <h2
             css={css`
               font-size: 1.8rem;
               padding: 0 1.5rem;
-              margin: 1rem 0;
+              margin: 1rem 0 0.5rem;
             `}
           >
             Articles
